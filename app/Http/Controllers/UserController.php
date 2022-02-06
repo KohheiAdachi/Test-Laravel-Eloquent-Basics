c80523df27e65c42fc1e3b0f924e3d9ad6e7c80b
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2022-02-06 19:01:55 +0900

    task8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,8 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
-
+        $users = User::whereIn('id',$request->users);
+        $users->delete();
         return redirect('/')->with('success', 'Users deleted');
     }
 
