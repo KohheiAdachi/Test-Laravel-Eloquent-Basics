61aeefc7a942915b1b30af29e5dcfdd85c2e11e9
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2022-02-06 18:58:29 +0900

    task 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,7 +48,11 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::where('name')
+                    ->updateOrCreate(['email' => $email,
+                                      'name' => $name,
+                                      'password' => Hash::make(Str::random(8))
+                                    ]); // updated or created user
 
         return view('users.show', compact('user'));
     }
