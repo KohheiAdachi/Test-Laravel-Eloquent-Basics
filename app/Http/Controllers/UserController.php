cd3d6d9ed44004e911df2e3fddb5e803ac2a515a
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2022-02-06 18:46:07 +0900

    task4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -31,7 +33,13 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::where('name', $name)
+                    ->where('email', $email)
+                    ->firstOrCreate([
+                        'name' => $name,
+                        'email' => $email,
+                        'password' => Hash::make(Str::random(8))
+                    ]);
 
         return view('users.show', compact('user'));
     }
